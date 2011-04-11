d7a7a05b44cc1b62c6bcee530501ea8e2cf6476d
Vojtech Juranek <vjuranek@redhat.com>
2011-04-11 10:07:46 +0200

    Fixed JAXBContext.newInstance() - specify full package name to avoid "doesnt contain ObjectFactory.class or jaxb.index" exception

--- DeltaCloudClient/src/org/deltacloud/client/DeltaCloudClient.java
+++ DeltaCloudClient/src/org/deltacloud/client/DeltaCloudClient.java
@@ -73,7 +73,8 @@ public class DeltaCloudClient implements API
 			this.username = username;
 			this.password = password;
 
-			JAXBContext jc = JAXBContext.newInstance(ObjectFactory.class.getPackage().getName());
+			//JAXBContext jc = JAXBContext.newInstance(ObjectFactory.class.getPackage().getName());
+			JAXBContext jc = JAXBContext.newInstance(org.deltacloud.ObjectFactory.class);
 
 			unmarshaller = jc.createUnmarshaller();
 			unmarshaller.setSchema(null);
